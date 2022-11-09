3699af1ec8b6a21a8861f6656429ba5a97a0d680
llpupp <llpupp@hotmail.com>
2022-11-09 16:34:06 -0300

    Initial commit

---
+++ src/BuscaSequencial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BuscaSequencial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int[] elementos = {64, 137, -16, 43, 67, 81, -90, 212, 10, 75};
+
+        System.out.println(elementos.length);
+        /*int count = 0;
+        while (count <= (elementos.length-1)) {
+            System.out.print(elementos[count] + "  ");
+            count++;
+        }*/
+
+
+
+
+    }
+}
